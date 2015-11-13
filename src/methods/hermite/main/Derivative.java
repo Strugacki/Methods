05f5f9ff8d0acd04e708c1dd9903091ce91852f2
Strugacki <mhallman94@gmail.com>
2015-11-13 12:53:01 +0100

    Small changes

--- src/methods/hermite/main/Derivative.java
+++ src/methods/hermite/main/Derivative.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
 
 public class Derivative {
-    // Function of 1 variable, keep track of 3 derivatives with respect to that variable,
-	// use 2.5 as the current value.  Basically, the identity function.
 	private double x;
 	private int a;
 	private DerivativeStructure polymonStruct;
@@ -16,28 +14,36 @@ public class Derivative {
 	private DerivativeStructure free;
 	private DerivativeStructure polymon;
 			
-			public Derivative(Point point){
-				this.x=point.getX();
-				this.a=point.getA();
-				polymonStruct = new DerivativeStructure(1,a, 0, x);
-				x3 = polymonStruct.pow(3);
-				x2 = polymonStruct.pow(2);
-				x1 = polymonStruct.pow(1);
-				free = polymonStruct.pow(0);
-				polymon = new DerivativeStructure(5.0, x3, 3.0, x2, 2.0, x1, 1.0,free);
-			}
+	/**
+	 * Constructor with argument
+	 * @param point
+	 */
+	public Derivative(Point point){
+		this.x=point.getX();
+		this.a=point.getA();
+		polymonStruct = new DerivativeStructure(1,a, 0, x);
+		x3 = polymonStruct.pow(3);
+		x2 = polymonStruct.pow(2);
+		x1 = polymonStruct.pow(1);
+		free = polymonStruct.pow(0);
+		//function is 5x^3 + 3x^3 + 2x + 1
+		polymon = new DerivativeStructure(5.0, x3, 3.0, x2, 2.0, x1, 1.0,free);
+	}
 
-			
-			public ArrayList<Double> makeDerivatives(){
-				ArrayList<Double> listY = new ArrayList<Double>();
-				for(int i = 1; i<=this.a;i++){
-					double value=this.polymon.getPartialDerivative(i);
-					System.out.println("Partial derivative number:"+this.a+" for function in point: "+this.x+" is ="+value);
-					listY.add(value);
-				}
-				
-				
-				
-				return listY;
-			}
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<Double> makeDerivatives(){
+		ArrayList<Double> listY = new ArrayList<Double>();
+		for(int i = 1; i<=this.a;i++){
+			double value=this.polymon.getPartialDerivative(i);
+			System.out.println("Partial derivative number:"+this.a+" for function in point: "+this.x+" is ="+value);
+			listY.add(value);
+		}
+		
+		
+		
+		return listY;
+	}
 }
