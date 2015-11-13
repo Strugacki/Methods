b7c2b7872f9f695830ac714fa1fa72d6a57400af
Strugacki <mhallman94@gmail.com>
2015-11-13 11:38:12 +0100

    app class added

--- src/methods/hermite/main/Interpolation.java
+++ src/methods/hermite/main/Interpolation.java
@@ -1,16 +1,12 @@
 package methods.hermite.main;
 
 public class Interpolation {
+	
+	
+	
+	
+	
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Hermite hermite = new Hermite();
-		hermite.readPoints();
-		System.out.println("Wybany przedzia³ to ["+hermite.getP()+","+hermite.getK()+"]");
-		for(int i=0;i<hermite.getPointsNumber();i++){
-			System.out.println(hermite.getPoint(i));
-		}
-
-	}
 
 }
